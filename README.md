25f98782e8c17b3e57ce8378e51507246256e6c4
tk-climb <hank9323@gmail.com>
2023-08-08 20:21:33 +0900

    1

--- README.md
+++
@@ -1,2 +0,0 @@
-# withTK
-# withTK
--- docker-ELK.sh
+++
@@ -1,4 +0,0 @@
-docker run -d -p 9200:9200 -p 9300:9300 --name elasticsearch docker.elastic.co/elasticsearch/elasticsearch:7.14.0
-docker run -d --link elasticsearch:elasticsearch --name logstash -v /path/to/logstash/config:/usr/share/logstash/config docker.elastic.co/logstash/logstash:7.14.0
-docker run -d -p 5601:5601 --link elasticsearch:elasticsearch --name kibana docker.elastic.co/kibana/kibana:7.14.0
-docker run -d --name filebeat --user=root --volume="/root/elk/filebeat.yml:/usr/share/filebeat/filebeat.yml:ro" --volume="/var/lib/docker/containers:/var/lib/docker/containers:ro" docker.elastic.co/beats/filebeat:7.14.0 -e
